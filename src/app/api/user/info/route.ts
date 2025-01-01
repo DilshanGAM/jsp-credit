a0138f964d7c8006671c146abcda9b191711faa5
Malith Dilshan <it21180934@my.sliit.lk>
2025-01-01 17:04:06 +0530

    user finding api✅

---
+++ src/app/api/user/info/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest,NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export async function GET(req:NextRequest){
+    
+    const user = req.headers.get("user");
+    
+    const query = req.nextUrl.searchParams.get("query")||"";
+
+    const nic = req.nextUrl.searchParams.get("nic")||"";
+
+    if(user){
+        const userObj = JSON.parse(user);
+        if(userObj.role === "admin"||userObj.role === "manager"||userObj.role === "staff"){
+            const foundUser = await prisma.user.findFirst({
+                where:{
+                    nic : nic
+                }});
+            if(foundUser){
+                return NextResponse.json(foundUser);
+            }else
+                return NextResponse.json({message:"User not found"},{status:404});       
+        }else{
+            return NextResponse.json({message:"Not Authorized"},{status:403});
+        }
+    }
+    
+}
